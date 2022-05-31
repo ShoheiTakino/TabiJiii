217bf175e1050a3ee04d1cc0491082496cda83cd
ShoheiTakino <shohei0522penguin@gmail.com>
2022-05-31 20:45:56 +0900

    新規投稿機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,22 @@ class PostsController < ApplicationController
   
   
   def index
-    @user = User.all
+    @user = User.all.order("created_at DESC")
     @posts = Post.all
   end
 
   def new
     @post = Post.new
     @user = Post.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -21,6 +30,11 @@ class PostsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :image, :content, :province_id).merge(user_id: current_user.id)
   end
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,14 @@
 class Post < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :province
+
 
   belongs_to :user
 
-  validates :title,     presence: true
+  has_one_attached :image
+
+  validates :title,       presence: true
   validates :content,     presence: true
-  validates :province_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image,       presence: true
+  validates :province_id, numericality: { other_than: 1, message: "can't be blank" }
 end
