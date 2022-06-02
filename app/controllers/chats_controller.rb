3881117e0e04cc14b34cfc9a177b785fa0d47f37
ShoheiTakino <shohei0522penguin@gmail.com>
2022-06-02 17:44:16 +0900

    チャット機能仮実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,20 @@
 class ChatsController < ApplicationController
+  
+  def new
+    @chat = Chat.new
+    @post = Post.find(params[:post_id])
+  end
+
+  def create
+    @chat = Chat.new
+    @post = Post.find(params[:post_id])
+    @chat = @post.chats.new(chat_params)
+    @chat.save
+  end
+
+  private 
+  def chat_params
+    params.require(:chat).permit(:comment).merge(user_id: current_user.id)
+  end
+
 end
